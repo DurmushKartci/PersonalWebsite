c509c4e1c3399dba3b3433c466356c712c9c7a0d
DurmushKartci <durmusikartci@gmail.com>
2021-08-02 01:11:35 +0300

    Logo için css ve js dosyaları eklendi ,iletişim için mali gönderme js eklendi,index.html hesar kısmı tamamlandı

---
+++ assets/js/logo.js
@@ -0,0 +1,18 @@
+let d = document.getElementById("d")
+let k = document.getElementById("k")
+let logo = document.getElementById("logo")
+let root = document.documentElement;
+
+function update_sizes() {
+    logo.style.height = logo.clientWidth + "px";
+    root.style.setProperty("--logo-font-size", (logo.clientWidth / 2.1) + "px")
+        // d.style.fontSize = (logo.clientWidth / 2.1) + "px";
+        // d.style.textShadow = (logo.clientWidth / 10) + " " + (logo.clientWidth / 10) + " " + (logo.clientWidth / 12) + "#9191916b";
+        // k.style.fontSize = (logo.clientWidth / 2.1) + "px";
+        // k.style.textShadow = (logo.clientWidth / 10) + " " + (logo.clientWidth / 10) + " " + (logo.clientWidth / 12) + "#9191916b";
+}
+
+
+let logo_size_interval = setInterval(function() {
+    update_sizes();
+}, 300);
